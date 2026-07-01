0c898187f776692323bade4738689b8314ae18fc
athakur190 <ashutosh.thakur190@gmail.com>
2026-06-30 22:25:05 -0700

    I changed the feature branch

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Ashutosh !
+        <h1 className="App-title">Welcome to React</h1>
+        <p className="App-intro">
+          I was changed on the Feature branch
         </p>
         <a
           className="App-link"
